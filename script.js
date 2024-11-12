94eafbbfd90eb35c00cfd7a1dfe4d56908063b55
icemnikol <ismailhisyammu@gmail.com>
2024-11-12 10:46:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+window.addEventListener("scroll", () => {
+   //buat smoth pas scroll navigasi
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+  //buat scrollpergantian navigasi
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+  
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// maju/mundur gambar
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+//kontrol gambaar
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
